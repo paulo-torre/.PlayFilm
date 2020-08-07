3e889ee2d907d61af3b4bb98d91654c7bbe63935
Marcelo Torre <pauloricardo.torre@gmail.com>
2020-08-07 19:59:03 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import Button from './components/Button';
+
+
+//import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="PlayFilms logo" />
+      </a>
+
+      <Button as="a" className="Button" href="/">
+        Nova Música
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
